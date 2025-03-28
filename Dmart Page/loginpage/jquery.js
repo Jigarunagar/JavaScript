171f1d10ddceb671c3e61b117390dc3370655ed3
Jigar Unagar <officialjigarunagar@gmail.com>
2025-03-28 21:06:37 +0530

    Add files via upload

---
+++ Dmart Page/loginpage/jquery.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconclose = document.querySelector('.icon-close');
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconclose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
