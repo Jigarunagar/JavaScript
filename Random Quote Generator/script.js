d08a53b2041f49e4d1dd7c54c8313512d07ed9de
Jigar Unagar <officialjigarunagar@gmail.com>
2025-05-16 17:52:02 +0530

    Update script.js

--- Random Quote Generator/script.js
+++ Random Quote Generator/script.js
@@ -25,23 +25,23 @@ const Text = document.getElementById("quote");
 const Author = document.getElementById("author");
 const btn = document.getElementById("nextcolor");
 
-function get() {
+const get = () => {
   const randomIndex = Math.floor(Math.random() * object.length);
   return object[randomIndex];
-}
+};
 
-function Background() {
+const Background = () => {
   const colors = ["#f39c12", "#8e44ad", "#16a085", "#2c3e50", "#c0392b"];
   const color = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = color;
-}
+};
 
-function display() {
+const display = () => {
   const quote = get();
   Text.textContent = `"${quote.text}"`;
   Author.textContent = `- ${quote.author}`;
   Background();
-}
+};
 
 btn.addEventListener("click", display);
 
